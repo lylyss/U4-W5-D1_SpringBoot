a84852ef600af4dd6892910df5ecd26040af535f
Loboda Nicolae <77647358+lylyss@users.noreply.github.com>
2025-07-14 17:07:49 +0200

    Add toppings list to Menu class

--- src/main/java/epicode/U4W5D1_SPRING/Entities/Menu.java
+++ src/main/java/epicode/U4W5D1_SPRING/Entities/Menu.java
@@ -4,11 +4,13 @@ import java.util.List;
 
 public class Menu {
     private List<Pizza> pizzas;
+    private List<Topping> toppings;
     private List<Drink> drinks;
 
-    public Menu(List<Pizza> pizzas, List<Drink> drinks) {
+    public Menu(List<Pizza> pizzas, List<Drink> drinks, List<Topping> toppings) {
         this.pizzas = pizzas;
         this.drinks = drinks;
+        this.toppings = toppings;
     }
 
     public void printMenu() {
@@ -17,6 +19,11 @@ public class Menu {
         System.out.println("PIZZE:");
         for (Pizza pizza : pizzas) {
             System.out.println("- " + pizza.getName() + " | Prezzo: €" + pizza.getPrice() + " | Calorie: " + pizza.getCalories());
+        }
+        System.out.println("AGGIUNTE");
+
+        for (Topping topping : toppings) {
+            System.out.println("- " + topping.getName() + "| Prezzo: €" + topping.getPrice() + " | Calorie: " + topping.getCalories());
         }
 
 
